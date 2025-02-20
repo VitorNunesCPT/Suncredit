4080bfc274447b6a225ff4e6087c88201e1864ce
VitorNunesCPT <vitornunescpt@gmail.com>
2025-02-19 23:09:19 -0300

    teste

--- App.tsx
+++ App.tsx
@@ -1,37 +1,24 @@
 import { ThemeProvider } from "@shopify/restyle";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/modules/auth/contexts/AuthContext";
+import { Routes } from "./src/routes";
 import theme from "./src/theme/theme";
-import Text from "./src/components/Text/Text";
-import Box from "./src/components/Box/Box";
-import { Button } from "./src/components/Button/Button";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView
-        style={{ flex: 1, backgroundColor: theme.colors.mainBackground }}
-      >
-        <Box flex={1} padding="s12" justifyContent="center" alignItems="center">
-          <Box marginBottom="s12">
-            <Text preset="headingLarge" color="primary" textAlign="center">
-              SunCredit
-            </Text>
-            <Text preset="paragraphMedium" textAlign="center">
-              Energia solar acessível para todos
-            </Text>
-          </Box>
-
-          <Box gap="s12">
-            <Button title="Começar Agora" variant="primary" size="large" />
-
-            <Button title="Saiba Mais" variant="outline" size="medium" />
-          </Box>
-
-          <StatusBar style="auto" />
-        </Box>
-      </SafeAreaView>
+      <NavigationContainer>
+        <AuthProvider>
+          <SafeAreaView
+            style={{ flex: 1, backgroundColor: theme.colors.mainBackground }}
+          >
+            <Routes />
+            <StatusBar style="auto" />
+          </SafeAreaView>
+        </AuthProvider>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
